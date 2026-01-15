607cb817fd1d10c68c37eded783ca58d8f9b04e1
GarenaFFus20 <alaminhossain1000a@gmail.com>
2026-01-15 10:25:06 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import Root from './components/Root'
 
 const App = () => {
   return (
     <div>
-          <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Root/>
 
     </div>
   )
